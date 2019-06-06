8a5064c14afdd894ad4fea6c4acaa2cea36cf824
Alexander Sakhaev <alsakhaev@gmail.com>
2019-06-06 16:59:46 +0300

    removed unused code

--- src/background/api/DappletRegistry.ts
+++ src/background/api/DappletRegistry.ts
@@ -2,24 +2,11 @@ export default class DappletRegistry {
 
     // ToDo: Load features from blockchain
     async getFeaturesByHostname(hostname: string): Promise<{ family: string, feature: string }[]> {
-        const response = await fetch('/examples/registry.json');
-        const json = await response.json();
-        if (json && json[hostname] && json[hostname].length > 0) {
-            return json[hostname];
-        } else {
-            return [];
-        }
+        return [];
     }
 
     // ToDo: Load scripts from swarm || ipfs
     async getScriptById(id: string): Promise<ArrayBuffer> {
-        try {
-            const response = await fetch('/examples/' + id + '.js');
-            if (!response.ok) return null;
-            const buffer = await response.arrayBuffer();
-            return buffer;
-        } catch {
-            return null;
-        }
+        return null;
     }
 }
