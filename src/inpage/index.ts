45727bcb61b503ad782650370032cef3bc244749
Alexander Sakhaev <alsakhaev@gmail.com>
2019-10-28 16:54:07 +0300

    optimize modules loading

--- src/inpage/index.ts
+++ src/inpage/index.ts
@@ -1,3 +1,9 @@
 import { init } from './injector'
 
-init().catch((err) => console.error("[Dapplet Extension] " + err));
+var observer = new MutationObserver(() => {
+    if (document.body) {
+        init().catch((err) => console.error("[Dapplet Extension] " + err));
+        observer.disconnect();
+    }
+});
+observer.observe(document.documentElement, { childList: true });
