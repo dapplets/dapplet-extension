9ec671221abe9fa5f2d6ca8f26c982e32fc8b322
Alexander Sakhaev <alsakhaev@gmail.com>
2019-10-29 15:50:45 +0300

    fake events

--- src/background/services/eventService.ts
+++ src/background/services/eventService.ts
@@ -2,5 +2,16 @@ import { Event } from "../../common/types";
 import { generateGuid } from '../../common/utils';
 
 export async function getEvents(): Promise<Event[]> {
-    return [];
+    const events: Event[] = [];
+
+    for (let i = 0; i < 100; i++) {
+        events.push({
+            id: generateGuid(),
+            title: "Title of test event #" + (i + 1),
+            description: "Here is description",
+            created: new Date()
+        });
+    }
+
+    return events;
 }
