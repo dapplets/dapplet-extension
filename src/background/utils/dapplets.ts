63e8840d461e8c41aab9934a626b82a998f6b47d
Alexander Sakhaev <alsakhaev@gmail.com>
2019-12-20 19:45:45 +0300

    fix tx building

--- src/background/utils/dapplets.ts
+++ src/background/utils/dapplets.ts
@@ -53,7 +53,7 @@ function builderTxSolidity(tx: any, data: any) {
     return {
         to: tx.to,
         data: fnSignature + valuesData.substring(2),
-        value: tx.value || "0x"
+        value: tx.value || 0
     };
 }
 
