ca41507a4e78aca169ae06382f17a34cdb504e01
Alexander Sakhaev <alsakhaev@gmail.com>
2019-12-25 18:35:09 +0300

    fix onMessage listener

--- src/inpage/index.ts
+++ src/inpage/index.ts
@@ -6,17 +6,17 @@ var observer = new MutationObserver(() => {
     if (document.body) {
         const core = new Core(); // ToDo: is it global for all modules?
         const injector = new Injector(core);
-        extension.runtime.onMessage.addListener((message, sender, sendResponse) => {
+        extension.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
             if (!message || !message.type) return;
 
             if (message.type === "FEATURE_ACTIVATED") {
                 const feature = message.payload
                 console.log(`The feature ${feature.name}#${feature.branch}@${feature.version} was activated.`, );
-                injector.loadModules([feature]);
+                await injector.loadModules([feature]);
             } else if (message.type === "FEATURE_DEACTIVATED") {
                 const feature = message.payload
                 console.log(`The feature ${feature.name}#${feature.branch}@${feature.version} was deactivated.`, );
-                injector.unloadModules([feature]);
+                await injector.unloadModules([feature]);
             } else if (message.type === "CURRENT_CONTEXT_IDS") {
                 sendResponse(injector.availableContextIds);
             }
