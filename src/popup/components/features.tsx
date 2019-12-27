eab3803687b50e4f7f9bbfda928f482f589a8fce
Alexander Sakhaev <alsakhaev@gmail.com>
2019-12-27 14:40:51 +0300

    error about connection lost

--- src/popup/components/features.tsx
+++ src/popup/components/features.tsx
@@ -6,13 +6,14 @@ import { Button, Image, List, Checkbox, Segment, Message, Popup } from "semantic
 import ManifestDTO from "../../background/dto/manifestDTO";
 
 interface IFeaturesProps {
-  contextIds: string[];
+  contextIds: string[] | undefined;
 }
 
 interface IFeaturesState {
   features: ManifestDTO[];
   isLoading: boolean;
   error: string;
+  isNoInpage: boolean;
 }
 
 class FeatureList extends React.Component<IFeaturesProps, IFeaturesState> {
@@ -22,15 +23,22 @@ class FeatureList extends React.Component<IFeaturesProps, IFeaturesState> {
     this.state = {
       features: [],
       isLoading: true,
-      error: null
+      error: null,
+      isNoInpage: false
     };
   }
 
   async componentDidMount() {
+    const { contextIds } = this.props;
+    if (contextIds === undefined) {
+      this.setState({ isNoInpage: true, isLoading: false });
+      return;
+    }
+
     const { getFeaturesByHostnames } = await initBGFunctions(extension);
 
     try {
-      const features: ManifestDTO[] = await getFeaturesByHostnames(this.props.contextIds);
+      const features: ManifestDTO[] = await getFeaturesByHostnames(contextIds);
       this.setState({
         features,
         isLoading: false,
@@ -67,75 +75,77 @@ class FeatureList extends React.Component<IFeaturesProps, IFeaturesState> {
   }
 
   render() {
-    const { features, isLoading, error } = this.state;
+    const { features, isLoading, error, isNoInpage } = this.state;
     return (
       <React.Fragment>
         <Segment loading={isLoading} className="internalTab">
-          {(!error) ? ((features.length > 0) ? (
-            <List divided relaxed style={{ width: 350 }}>
-              {features.map(f => (
-                <List.Item key={f.name} style={{ overflow: "hidden" }}>
-                  <List.Content style={{ width: 45, float: "left" }}>
-                    <div>
-                      <Popup
-                        content={<List>{f.hostnames?.map(h => <List.Item>{h}</List.Item>)}</List>}
-                        header="Related Context IDs"
-                        trigger={<Image
-                          size="mini"
-                          avatar
-                          alt={f.description}
-                          src={f.icon}
-                        />}
-                      />                      
-                    </div>
-                  </List.Content>
-                  {false ? ( // ToDo: hasUpdate isn't implemented in DTO
-                    <List.Content style={{ float: "right", width: 60 }}>
-                      <Button
-                        primary
-                        size="mini"
-                        style={{ padding: 5, width: 55 }}
-                      >
-                        Update
-                    </Button>
-                      <Button
-                        size="mini"
-                        style={{ padding: 5, marginTop: 5, width: 55 }}
-                      >
-                        Skip
-                    </Button>
-                    </List.Content>
-                  ) : (
-                      <List.Content style={{ float: "right", width: 60 }}>
-                        <Checkbox
-                          toggle
-                          style={{ marginTop: 5 }}
-                          onChange={() =>
-                            this.handleSwitchChange(f, !f.isActive)
-                          }
-                          checked={f.isActive}
+          {!isNoInpage ?
+            (!error) ? ((features.length > 0) ? (
+              <List divided relaxed style={{ width: 350 }}>
+                {features.map(f => (
+                  <List.Item key={f.name} style={{ overflow: "hidden" }}>
+                    <List.Content style={{ width: 45, float: "left" }}>
+                      <div>
+                        <Popup
+                          content={<List>{f.hostnames?.map(h => <List.Item>{h}</List.Item>)}</List>}
+                          header="Related Context IDs"
+                          trigger={<Image
+                            size="mini"
+                            avatar
+                            alt={f.description}
+                            src={f.icon}
+                          />}
                         />
+                      </div>
+                    </List.Content>
+                    {false ? ( // ToDo: hasUpdate isn't implemented in DTO
+                      <List.Content style={{ float: "right", width: 60 }}>
+                        <Button
+                          primary
+                          size="mini"
+                          style={{ padding: 5, width: 55 }}
+                        >
+                          Update
+                    </Button>
+                        <Button
+                          size="mini"
+                          style={{ padding: 5, marginTop: 5, width: 55 }}
+                        >
+                          Skip
+                    </Button>
                       </List.Content>
-                    )}
-                  <List.Content
-                    style={{
-                      marginLeft: 45,
-                      marginRight: 60
-                    }}
-                  >
-                    <List.Header>{f.title}</List.Header>
-                    <List.Description style={{ color: "#666" }}>
-                      {f.description}
-                      <br />
-                      Author: {f.author}
-                      <br />
-                      Version: {f.version}
-                    </List.Description>
-                  </List.Content>
-                </List.Item>
-              ))}
-            </List>
-          ) : (<div>No available features for current site.</div>)) : (<Message floating negative>{error}</Message>)}
+                    ) : (
+                        <List.Content style={{ float: "right", width: 60 }}>
+                          <Checkbox
+                            toggle
+                            style={{ marginTop: 5 }}
+                            onChange={() =>
+                              this.handleSwitchChange(f, !f.isActive)
+                            }
+                            checked={f.isActive}
+                          />
+                        </List.Content>
+                      )}
+                    <List.Content
+                      style={{
+                        marginLeft: 45,
+                        marginRight: 60
+                      }}
+                    >
+                      <List.Header>{f.title}</List.Header>
+                      <List.Description style={{ color: "#666" }}>
+                        {f.description}
+                        <br />
+                        Author: {f.author}
+                        <br />
+                        Version: {f.version}
+                      </List.Description>
+                    </List.Content>
+                  </List.Item>
+                ))}
+              </List>
+            ) : (<div>No available features for current site.</div>)) : (<Message floating negative>{error}</Message>)
+            : (<div>No connection with context webpage. <br/> Please refresh it.</div>)}
         </Segment>
       </React.Fragment>
     );
