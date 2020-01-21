8c54b47a82a757c7444b912199babc6709fca2bf
Dmitry Palchun <dima@ethernian.com>
2020-01-21 18:42:33 +0100

    [-]  excessive semicolons removed

--- src/inpage/connection.ts
+++ src/inpage/connection.ts
@@ -22,28 +22,28 @@ const ANY_EVENT: any = Symbol('any_event')
 const TYPE_FILTER = (type: string) => (op: any, msg: any) => msg.type === type
 
 export interface IConnection {
-    readonly listeners: Map<Key, Listener>;
-    send(op: any, msg: any): Promise<any>;
+    readonly listeners: Map<Key, Listener>
+    send(op: any, msg: any): Promise<any>
     subscribe(topic: string, message?: any): Key
-    listen(h: EventHandler): this;
-    listen(f: MsgFilter, ap?: AutoProperty[]): this;
-    listen(f: MsgFilter, h: MsgHandler, ap?: AutoProperty[]): this;
-    listen(f: MsgFilter, h: EventHandler, ap?: AutoProperty[]): this;
+    listen(h: EventHandler): this
+    listen(f: MsgFilter, ap?: AutoProperty[]): this
+    listen(f: MsgFilter, h: MsgHandler, ap?: AutoProperty[]): this
+    listen(f: MsgFilter, h: EventHandler, ap?: AutoProperty[]): this
     listener(h: EventHandler): Key
     listener(f: MsgFilter, ap?: AutoProperty[]): Key
     listener(f: MsgFilter, h: MsgHandler, ap?: AutoProperty[]): Key
     listener(f: MsgFilter, h: EventHandler, ap?: AutoProperty[]): Key
-    unsubscribe(nn: number): void;
+    unsubscribe(nn: number): void
     addAutoProperties(f: MsgFilter, ap: AutoProperty[]): Key
-    topicMatch(topic: string, pattern: string): boolean;
-    onMessage(op: any, msg: any): void;
+    topicMatch(topic: string, pattern: string): boolean
+    onMessage(op: any, msg: any): void
     get(key: any): Key
-    set(key: any, value: any): void;
+    set(key: any, value: any): void
 }
 
 export class Connection implements IConnection {
-    private _ctxNNmap = new WeakMap<any, number>();
-    private _nnSubscriptionMap = new Map<Key, any>();
+    private _ctxNNmap = new WeakMap<any, number>()
+    private _nnSubscriptionMap = new Map<Key, any>()
     private autoProperties = new Map<Key, AutoProperty>()
     public readonly listeners = new Map<Key, Listener>()
     private nn = 0 //a numeric handle counter to address listeners and autopropertes
@@ -56,16 +56,16 @@ export class Connection implements IConnection {
     // op - operation, subject
     // msg - payload
     send(op: any, msg: any) { // should return promise
-        return this._bus.exec(op, msg);
+        return this._bus.exec(op, msg)
     }
 
     subscribe(topic: string, message?: any): Key {
         const nn = this.listener(topic); // topic === tweetInfo
         const subscription = this._bus.subscribe(topic, [message], (result) => {
-            this.onMessage(topic, result, nn);
-        });
-        this._nnSubscriptionMap.set(nn, subscription);
-        return nn;
+            this.onMessage(topic, result, nn)
+        })
+        this._nnSubscriptionMap.set(nn, subscription)
+        return nn
     }
 
     listen(h: EventHandler): this
@@ -73,8 +73,8 @@ export class Connection implements IConnection {
     listen(f: MsgFilter, h: MsgHandler, ap?: AutoProperty[]): this
     listen(f: MsgFilter, h: EventHandler, ap?: AutoProperty[]): this
     listen(filterOrHander: MsgFilter | EventHandler, evtOrMsgOrAP?: EventHandler | MsgHandler | AutoProperty[], ap?: AutoProperty[]): this {
-        this.listener.call(this, arguments);
-        return this;
+        this.listener.call(this, arguments)
+        return this
     }
 
     // call, when new context was created.
@@ -95,34 +95,34 @@ export class Connection implements IConnection {
                 this.listeners.set(++this.nn, { f: filterOrHander, h: evtOrMsgOrAP!, p: ap })
             }
         }
-        return this.nn;
+        return this.nn
     }
 
     unsubscribe(nn: number) {
-        this.listeners.delete(nn);
-        this._nnSubscriptionMap.get(nn).unsubscribe();
+        this.listeners.delete(nn)
+        this._nnSubscriptionMap.get(nn).unsubscribe()
     }
 
     //connection with AutoProperty support added by proxy
     static create<M>(_bus: IPubSub, eventsDef?: EventDef<any>): AutoProperties<M> & Connection {
-        const conn = new Connection(_bus, eventsDef);
+        const conn = new Connection(_bus, eventsDef)
         return new Proxy(conn, {
             get(target: any, prop, receiver) {
                 //let idx: number = 0
 
-                if (prop in target) return target[prop];
+                if (prop in target) return target[prop]
 
                 const autoProperty: AutoProperty = {
                     name: prop as string,
                     set: (ctx: any, setter: (value: any) => void) => {
-                        const nn = target.get(ctx);
-                        const listener = target.listeners.get(nn);
-                        if (!listener.h) listener.h = { [ANY_EVENT]: [] };
-                        listener.h[ANY_EVENT].push((op, msg) => setter(msg[prop]));
+                        const nn = target.get(ctx)
+                        const listener = target.listeners.get(nn)
+                        if (!listener.h) listener.h = { [ANY_EVENT]: [] }
+                        listener.h[ANY_EVENT].push((op, msg) => setter(msg[prop]))
                     }
                 }
 
-                return autoProperty;
+                return autoProperty
             }
         })
     }
@@ -138,8 +138,8 @@ export class Connection implements IConnection {
     }
 
     topicMatch(topic: string, pattern: string): boolean {
-        if (!pattern || pattern == topic) return true;
-        else if (!topic) return false;
+        if (!pattern || pattern == topic) return true
+        else if (!topic) return false
 
         let expected = pattern.split('.')
         let actual = topic.split('.')
@@ -156,7 +156,7 @@ export class Connection implements IConnection {
         const isTopicMatch = (op: any, msg: any, f: MsgFilter) =>
             typeof f === 'string' ? this.topicMatch(op, f) : f(op, msg)
 
-        const listeners = nn ? [this.listeners.get(nn)] : this.listeners;
+        const listeners = nn ? [this.listeners.get(nn)] : this.listeners
 
         listeners.forEach((listener) => {
             if (!listener.f || isTopicMatch(op, msg, listener.f)) {
@@ -165,7 +165,7 @@ export class Connection implements IConnection {
                         let cond = this.eventDef ? this.eventDef[eventId] : eventId
                         //ToDo: extract msg.type default
                         if (typeof cond === 'function' ? cond(op, msg) : msg.type == cond) {
-                            const handlers = listener.h[eventId];
+                            const handlers = listener.h[eventId]
                             if (Array.isArray(handlers)) {
                                 handlers.forEach(h => h(op, msg))
                             } else {
@@ -173,7 +173,7 @@ export class Connection implements IConnection {
                             }
                         }
                         if (eventId === ANY_EVENT) {
-                            const handlers = listener.h[eventId];
+                            const handlers = listener.h[eventId]
                             if (Array.isArray(handlers)) {
                                 handlers.forEach(h => h(op, msg))
                             } else {
@@ -195,10 +195,10 @@ export class Connection implements IConnection {
     }
 
     get(key: any): number {
-        return this._ctxNNmap.get(key);
+        return this._ctxNNmap.get(key)
     }
 
     set(key: any, value: any): void {
-        this._ctxNNmap.set(key, value);
+        this._ctxNNmap.set(key, value)
     }
 }
