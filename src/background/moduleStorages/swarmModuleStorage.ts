d3d842c4869b9d8b34db95f2c4487573323978e9
Alexander Sakhaev <alsakhaev@gmail.com>
2020-04-20 18:17:12 +0300

    feat: modules hash checking

--- src/background/moduleStorages/swarmModuleStorage.ts
+++ src/background/moduleStorages/swarmModuleStorage.ts
@@ -1,7 +1,11 @@
 import { Storage as ModuleStorage } from './storage';
+import * as ethers from 'ethers';
 
 export class SwarmModuleStorage implements ModuleStorage {
     public async getResource(uri: string): Promise<ArrayBuffer> {
+        const url = new URL(uri);
+        const expectedHash = url.hash.substring(1);
+
         const response = await fetch("https://swarm-gateways.net/" + uri);
 
         if (!response.ok) {
@@ -9,6 +13,18 @@ export class SwarmModuleStorage implements ModuleStorage {
         }
 
         const buffer = await response.arrayBuffer();
+        const keccak = ethers.utils.keccak256(new Uint8Array(buffer)).substring(2);
+        
+        if (!!expectedHash) {
+            if (keccak !== expectedHash) {
+                console.error(`Hash is not valid. URL: ${uri}, expected: ${expectedHash}, recieved: ${keccak}`);
+                throw Error('Hash is not valid.');
+            } else {
+                console.log(`Successful hash checking. URL: ${uri}, expected: ${expectedHash}, recieved: ${keccak}`);
+            }
+        } else {
+            console.warn(`Skiped hash checking. URL: ${uri}`);
+        }
 
         return buffer;
     }
