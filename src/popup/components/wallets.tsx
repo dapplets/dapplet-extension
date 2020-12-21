faf91601147469257461437e2baf91ee79c668fa
Alexander Sakhaev <alsakhaev@gmail.com>
2020-12-21 15:27:15 +0300

    improvement: similar ui of paired wallets

--- src/popup/components/wallets.tsx
+++ src/popup/components/wallets.tsx
@@ -74,35 +74,33 @@ class Wallets extends React.Component<IWalletsProps, IWalletsState> {
     return (
       <React.Fragment>
         <Segment className="internalTab">
-          {(connectedDescriptors.length > 0) ? 
-          <Comment.Group>
-            {connectedDescriptors.map((x, i) => (
-              <Comment key={i}>
-                {(x.account) ? <Comment.Avatar src={makeBlockie(x.account)} /> : null}
-                <Comment.Content>
-                  <Comment.Author style={{ display: 'inline' }}>
-                    {(x.default) ? <Icon name='star' /> : <Icon link name='star outline' onClick={() => this.setWalletFor(x.type)} />}
-                    {(x.account) ? x.account.substr(0, 6) + '...' + x.account.substr(38) : null}
-                  </Comment.Author>
-                  {/* <Comment.Author style={{ display: 'inline' }}>{x.account}</Comment.Author> */}
-                  <Comment.Metadata>
-                    <CheckIcon text='Copied' name='copy' onClick={() => navigator.clipboard.writeText(x.account)} />
-                    <Icon link name='external' onClick={() => window.open(`https://${(x.chainId === 1) ? '' : networkName(x.chainId) + '.'}etherscan.io/address/${x.account}`, '_blank')} />
-                    {(x.lastUsage) ? <ReactTimeAgo date={new Date(x.lastUsage)} locale="en-US"/> : null}
-                  </Comment.Metadata>
-                  <Comment.Text>
-                    {walletIcons[x.type] ? <img style={{ width: '16px' }} src={walletIcons[x.type]} /> : null}
-                    {x.meta?.icon ? <img style={{ width: '16px' }} src={x.meta.icon} /> : null}
-                    <span style={{ marginLeft: '0.5em' }}>{networkName(x.chainId)}</span>
-                  </Comment.Text>
-                  <Comment.Actions>
-                    <Comment.Action onClick={() => this.disconnectButtonClick(x.type)}>Disconnect</Comment.Action>
-                  </Comment.Actions>
-                </Comment.Content>
-              </Comment>
-            ))}
-          </Comment.Group> : 
-          <div style={{ marginBottom: '10px'}}>No connected wallets</div>}
+          {(connectedDescriptors.length > 0) ?
+              <Comment.Group>
+                {connectedDescriptors.map((x, i) => (
+                  <div key={i} style={{ marginBottom: '10px', display: 'flex', boxShadow: '0 0 0 1px rgba(34,36,38,.15) inset', borderRadius: '.28571429rem', padding: '.78571429em 1.5em .78571429em' }}>
+                    {(x.account) ? <img src={makeBlockie(x.account)} style={{ width: '38px', height: '38px', borderRadius: '4px', margin: '2px 0' }} /> : null}
+                    <div style={{ flex: 'auto', marginLeft: '10px' }}>
+                      <div style={{ display: 'inline', color: 'rgba(0,0,0,.4)' }}>
+                        {/* {(x.default) ? <Icon name='star' /> : <Icon link name='star outline' onClick={() => this.setWalletFor(x.type)} />} */}
+                        {(x.account) ? <span title={x.account} style={{ color: '#000', fontWeight: 'bold' }}>{x.account.substr(0, 6) + '...' + x.account.substr(38)}</span> : null}
+                        <CheckIcon text='Copied' name='copy' style={{ marginLeft: '4px' }} onClick={() => navigator.clipboard.writeText(x.account)} />
+                      </div>
+                      {/* <Comment.Author style={{ display: 'inline' }}>{x.account}</Comment.Author> */}
+                      {/* <Icon link name='external' onClick={() => window.open(`https://${(x.chainId === 1) ? '' : networkName(x.chainId) + '.'}etherscan.io/address/${x.account}`, '_blank')} /> */}
+                      <div>
+                        {walletIcons[x.type] ? <img style={{ width: '16px', position: 'relative', top: '3px' }} src={walletIcons[x.type]} /> : null}
+                        {x.meta?.icon ? <img style={{ width: '16px', position: 'relative', top: '3px', marginLeft: '3px' }} src={x.meta.icon} /> : null}
+                        {(x.lastUsage) ? <span style={{ marginLeft: '6px', color: 'rgba(0,0,0,.4)' }}><ReactTimeAgo date={new Date(x.lastUsage)} locale="en-US" /></span> : null}
+                        {/* <span style={{ marginLeft: '0.5em' }}>{networkName(x.chainId)}</span> */}
+                      </div>
+                    </div>
+                    <div>
+                      <Button onClick={() => this.disconnectButtonClick(x.type)} size='tiny' style={{ margin: '5px 0' }}>Disconnect</Button>
+                    </div>
+                  </div>
+                ))}
+              </Comment.Group> :
+            <div style={{ marginBottom: '10px' }}>No connected wallets</div>}
 
           <Button onClick={() => this.connectWallet()}>Connect</Button>
 
