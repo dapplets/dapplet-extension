4be9f832fbc3aa912060911ce1818f693fcd7dcb
Alexander Sakhaev <alsakhaev@gmail.com>
2021-05-27 15:44:30 +0300

    fix(popup): incorrectly detecting updates for prereleases

--- src/background/services/githubService.ts
+++ src/background/services/githubService.ts
@@ -1,6 +1,8 @@
+import { gt } from 'semver';
+
 export async function isExtensionUpdateAvailable() {
     const url = 'https://api.github.com/repos/dapplets/dapplet-extension/releases/latest';
     const resp = await fetch(url);
     const json = await resp.json();
-    return json.name !== 'v' + EXTENSION_VERSION;
+    return gt(json.name, EXTENSION_VERSION);
 }
