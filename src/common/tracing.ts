32346c3ffdf9a1464e3cababe151d286f4876acd
Alexander Sakhaev <alsakhaev@gmail.com>
2021-06-30 15:49:49 +0300

    refactor: ignore tracing of `ResizeObserver loop limit exceeded` error

--- src/common/tracing.ts
+++ src/common/tracing.ts
@@ -8,6 +8,7 @@ export function startTracing() {
         dsn: "https://41ee3f6e2cdc41b89f14b6377d56d1d5@o880231.ingest.sentry.io/5833728",
         integrations: [new Integrations.BrowserTracing()],
         release: `dapplet-extension@${EXTENSION_VERSION}`,
-        tracesSampleRate: 1.0
+        tracesSampleRate: 1.0,
+        ignoreErrors: ['ResizeObserver loop limit exceeded'],
     });
 }
