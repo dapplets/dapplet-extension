6af1ed80d3af0f25b6ac23a4f4153b208e9d1bc2
Alexander Sakhaev <alsakhaev@gmail.com>
2021-09-16 12:41:33 +0300

    fix: centralized storage doesn't save objects

--- src/background/moduleStorages/centralizedModuleStorage.ts
+++ src/background/moduleStorages/centralizedModuleStorage.ts
@@ -3,11 +3,14 @@ import { timeoutPromise } from '../../common/helpers';
 import { ethers } from 'ethers';
 
 type PresignedPost = { [key: string]: string };
+type PresignResponse = {
+    postURL: string;
+    formData: PresignedPost;
+};
 
 export class CentralizedModuleStorage implements ModuleStorage {
     public timeout = 5000;
     private _s3ReadEndpoint = "https://dapplet-api.s3.nl-ams.scw.cloud/";
-    private _s3WriteEndpoint = "https://dapplet-api.s3.nl-ams.scw.cloud/";
     private _authEndpoint = "https://dapplet-api.herokuapp.com/s3/presign";
 
     public async getResource(hash: string, fetchController: AbortController = new AbortController()): Promise<ArrayBuffer> {
@@ -30,9 +33,9 @@ export class CentralizedModuleStorage implements ModuleStorage {
         const buffer = await (blob as any).arrayBuffer();
         const hash = ethers.utils.keccak256(new Uint8Array(buffer)).replace('0x', '');
         
-        let presignedPost: PresignedPost = null;
+        let presignResponse: PresignResponse = null;
         try {
-            presignedPost = await this._createPresignedPost(hash);
+            presignResponse = await this._createPresignedPost(hash);
         } catch (err) {
             if (err.message.indexOf('Item with such ID already exists') !== -1) {
                 console.log(`Object "${hash}" already exists in centralized storage. Skipping uploading...`);
@@ -42,18 +45,18 @@ export class CentralizedModuleStorage implements ModuleStorage {
             }
         }
 
-        await this._createObject(blob, presignedPost, hash);
+        await this._createObject(blob, presignResponse, hash);
         return hash;
     }
 
     async saveDir(data: { files: { url: string, arr: ArrayBuffer }[], hash: string }): Promise<string> {
         const hash = data.hash.replace('0x', '');
-        const presignedPost = await this._createPresignedPost(hash);
-        await Promise.all(data.files.map(x => this._createObject(new Blob([x.arr]), presignedPost, hash + '/' + x.url)));
+        const presignResponse = await this._createPresignedPost(hash);
+        await Promise.all(data.files.map(x => this._createObject(new Blob([x.arr]), presignResponse, hash + '/' + x.url)));
         return data.hash;
     }
 
-    private async _createPresignedPost(id: string): Promise<PresignedPost> {
+    private async _createPresignedPost(id: string): Promise<PresignResponse> {
         const body = JSON.stringify({ id });
         const response = await fetch(this._authEndpoint, {
             method: 'POST',
@@ -63,16 +66,18 @@ export class CentralizedModuleStorage implements ModuleStorage {
 
         const json = await response.json();
         if (!json.success) throw new Error(json.message || "Cannot create presigned post for S3 storage");
-        return json.data.formData;
+        return json.data;
     }
 
-    private async _createObject(blob: Blob, presignedPost: PresignedPost, key: string) {
+    private async _createObject(blob: Blob, presignResponse: PresignResponse, key: string) {
         const form = new FormData();
         form.append('file', blob);
-        Object.entries(presignedPost).forEach(([k, v]) => form.set(k, v));
+        Object.entries(presignResponse.formData)
+            .filter(([k]) => k !== 'bucket')
+            .forEach(([k, v]) => form.set(k, v));
         form.set('key', key);
 
-        const response = await fetch(this._s3WriteEndpoint, {
+        const response = await fetch(presignResponse.postURL, {
             method: 'POST',
             body: form
         });
