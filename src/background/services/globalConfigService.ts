9f5c259fb2c9cfdaaa55a033b8db5e729302777f
Alexander Sakhaev <alsakhaev@gmail.com>
2021-10-20 20:15:21 +0300

    feat: the default trusted users are updated

--- src/background/services/globalConfigService.ts
+++ src/background/services/globalConfigService.ts
@@ -185,7 +185,9 @@ export default class GlobalConfigService {
             { account: "buidl.testnet" },
             { account: "nik3ter.testnet" },
             { account: "0x692a4d7B7BE2dc1623155E90B197a82D114a74f3" },
-            { account: "0x9126d36880905fcb9e5f2a7f7c4f19703d52bc62" }
+            { account: "0x9126d36880905fcb9e5f2a7f7c4f19703d52bc62" },
+            { account: "0xf64849376812667bda7d902666229f8b8dd90687" },
+            { account: "team.dapplet-base.eth" }
         ];
         config.userSettings = {};
         config.providerUrl = 'https://rinkeby.infura.io/v3/eda881d858ae4a25b2dfbbd0b4629992';
