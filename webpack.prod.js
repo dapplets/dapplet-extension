01f68f89a84cbfb90bebb2f782f45a0f28a0e607
Alexander Sakhaev <alsakhaev@gmail.com>
2022-06-01 15:09:30 +0300

    build: optimize development build

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,12 +1,28 @@
-const { merge } = require("webpack-merge");
-const common = require("./webpack.common.js");
-const webpack = require("webpack");
+const { merge } = require('webpack-merge')
+const common = require('./webpack.common.js')
+const webpack = require('webpack')
+const TerserPlugin = require('terser-webpack-plugin')
 
 module.exports = merge(common, {
-  mode: "production",
+  mode: 'production',
+  optimization: {
+    splitChunks: {
+      chunks(chunk) {
+        // exclude `inpage`
+        return chunk.name !== 'inpage'
+      },
+      name: 'common',
+    },
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        extractComments: false,
+      }),
+    ],
+  },
   plugins: [
     new webpack.DefinePlugin({
-      EXTENSION_ENV: JSON.stringify("production")
-    })
-  ]
-});
+      EXTENSION_ENV: JSON.stringify('production'),
+    }),
+  ],
+})
