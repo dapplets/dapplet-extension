c351a24d852801cacfab01f9dbcc241c311a6e36
LiSofa <lisofffa@gmail.com>
2022-08-10 01:49:58 +0300

    feat: add visible and deleted contextId

--- src/contentscript/overlay/root/pages/DappletsInfo/index.tsx
+++ src/contentscript/overlay/root/pages/DappletsInfo/index.tsx
@@ -3,7 +3,6 @@ import cn from 'classnames'
 import React, { FC, useEffect, useRef, useState } from 'react'
 import { browser } from 'webextension-polyfill-ts'
 import ModuleInfo from '../../../../../background/models/moduleInfo'
-import VersionInfo from '../../../../../background/models/versionInfo'
 import { StorageTypes } from '../../../../../common/constants'
 import { chainByUri, typeOfUri } from '../../../../../common/helpers'
 import { ChainTypes, DefaultSigners } from '../../../../../common/types'
@@ -26,7 +25,7 @@ export const DappletsMainInfo: FC<DappletsMainInfoProps> = (props) => {
   const { setDappletsDetail, ModuleInfo, ModuleVersion, setShowChildrenRegistery } = props
   const [originalMi, setOriginalMi] = useState(null)
   const [mi, setMi] = useState<ModuleInfo>(ModuleInfo)
-  const [vi, setVi] = useState<VersionInfo>(ModuleVersion)
+  // const [vi, setVi] = useState<VersionInfo>(ModuleVersion)
   const [targetRegistry, setTargetRegistry] = useState(null)
   const [targetChain, setTargetChain] = useState<ChainTypes>(null)
   const [message, setMessage] = useState(null)
@@ -62,11 +61,11 @@ export const DappletsMainInfo: FC<DappletsMainInfoProps> = (props) => {
   const [visible, setVisible] = useState(false)
   const nodeBtn = useRef<HTMLButtonElement>()
   const nodeInput = useRef<HTMLInputElement>()
-  const [visibleContextId, setVisibleContextId] = useState({
-    visibleContext: [],
-  })
+  const messagesContainer = useRef<HTMLDivElement>()
+
   const [contextDeleteNone, setContextDeleteNone] = useState(false)
   const [addDisabled, setAddDisabled] = useState(false)
+  const [contextId, setContextId] = useState(null)
 
   useEffect(() => {
     _isMounted = true
@@ -86,7 +85,7 @@ export const DappletsMainInfo: FC<DappletsMainInfoProps> = (props) => {
     return () => {
       _isMounted = false
     }
-  }, [mi, newState, targetChain, autorDisabled, author, editContextId])
+  }, [mi, newState, targetChain, autorDisabled, author, editContextId, contextId])
 
   const _updateData = async () => {
     const { getRegistries, getTrustedUsers } = await initBGFunctions(browser)
@@ -106,6 +105,8 @@ export const DappletsMainInfo: FC<DappletsMainInfoProps> = (props) => {
     setTargetChain(chainByUri(typeOfUri(prodRegistries[0]?.url ?? '')))
 
     await _updateCurrentAccount()
+    if (!targetRegistry) return
+    getContextId()
   }
   const _updateCurrentAccount = async () => {
     if (targetChain) {
@@ -193,25 +194,32 @@ export const DappletsMainInfo: FC<DappletsMainInfoProps> = (props) => {
     newAuthor.authorForm.push(newAuthorObject)
     setAuthor(newAuthor)
   }
-  const messagesContainer = useRef<HTMLDivElement>()
-
   const onDeleteChild = (id: number) => {
     const newAuthor = Object.assign({}, author)
     newAuthor.authorForm.splice(id, 1)
     setAuthor(newAuthor)
   }
 
-  const newContextObject = ''
-  const addButtonClickHandlerContext = () => {
-    const newContext = Object.assign({}, mi)
-    newContext.contextIds.push(newContextObject)
-    setMi(newContext)
-  }
+  const onDeleteContextId = async (context) => {
+    setEditContextIdLoading(true)
+    setAddDisabled(true)
+    try {
+      const { removeContextId } = await initBGFunctions(browser)
+      await removeContextId(targetRegistry, mi.name, context)
 
-  const onDeleteChildContext = (id: number) => {
-    const newContext = Object.assign({}, mi)
-    newContext.contextIds.splice(id, 1)
-    setMi(newContext)
+      setEditContextIdDone(true)
+      setVisible(true)
+      setEditContextIdLoading(false)
+      setAddDisabled(false)
+      nodeBtn.current?.classList.remove('valid')
+      setEditContextId('')
+    } catch (error) {
+      setEditContextIdDone(true)
+      setVisible(false)
+      setEditContextIdLoading(false)
+      setAddDisabled(false)
+      nodeBtn.current?.classList.remove('valid')
+    }
   }
 
   const _addContextId = async (contextId: string) => {
@@ -221,14 +229,9 @@ export const DappletsMainInfo: FC<DappletsMainInfoProps> = (props) => {
     try {
       const { addContextId } = await initBGFunctions(browser)
       await addContextId(targetRegistry, mi.name, contextId)
-      const newForm = Object.assign({}, visibleContextId)
-
-      newForm.visibleContext.push(editContextId)
-      setVisibleContextId(newForm)
 
       setEditContextIdDone(true)
       setVisible(true)
-      setEditContextId('')
       setEditContextIdLoading(false)
       setAddDisabled(false)
       nodeBtn.current?.classList.remove('valid')
@@ -246,6 +249,12 @@ export const DappletsMainInfo: FC<DappletsMainInfoProps> = (props) => {
     const lastFourCharacters = hash.substring(hash.length - 1, hash.length - 5)
 
     return `${firstFourCharacters}...${lastFourCharacters}`
+  }
+  const getContextId = async () => {
+    const { getContextIds } = await initBGFunctions(browser)
+
+    const newContextID = await getContextIds(targetRegistry, mi.name)
+    setContextId(newContextID)
   }
 
   return (
@@ -472,18 +481,18 @@ export const DappletsMainInfo: FC<DappletsMainInfoProps> = (props) => {
                 <div className={styles.blockContextID}>
                   <h3 className={styles.blockContextIDTitle}>Context IDs</h3>
                   <button
-                    disabled={mi.contextIds.length >= 1}
-                    onClick={addButtonClickHandlerContext}
-                    className={cn(styles.contextIDButton, {
-                      [styles.contextIDButtonDisabled]: mi.contextIds.length >= 1,
-                    })}
+                    onClick={() => setVisible(!visible)}
+                    className={cn(styles.contextIDButton, {})}
                   />
                 </div>
-                {mi.contextIds.map((x, i) => (
-                  <div key={i} className={styles.wrapperContext}>
+                {visible && (
+                  <div
+                    //  key={i}
+                    className={styles.wrapperContext}
+                  >
                     <div className={styles.blockContext}>
                       <input
-                        key={i}
+                        // key={i}
                         ref={nodeInput}
                         className={styles.blockContextTitle}
                         value={editContextId}
@@ -496,7 +505,7 @@ export const DappletsMainInfo: FC<DappletsMainInfoProps> = (props) => {
                       <button
                         ref={nodeBtn}
                         onClick={() => {
-                          onDeleteChildContext(i)
+                          // onDeleteChildContext(i)
                           setEditContextId('')
                         }}
                         className={cn(styles.contextDelete, {
@@ -518,21 +527,25 @@ export const DappletsMainInfo: FC<DappletsMainInfoProps> = (props) => {
                       ADD
                     </button>
                   </div>
-                ))}
+                )}
                 {editContextIdLoading ? (
                   <div className={styles.editContextIdLoading}></div>
                 ) : (
                   <>
-                    {visible &&
-                      visibleContextId &&
-                      visibleContextId.visibleContext.map((x, i) => (
-                        <input
-                          key={i}
-                          className={styles.blockContext}
-                          placeholder={x}
-                          value={x}
-                          readOnly
-                        />
+                    {contextId &&
+                      contextId.map((x, i) => (
+                        <div key={i} className={styles.blockAuthors}>
+                          <input
+                            className={cn(styles.blockContext, styles.blockContextValue)}
+                            placeholder={x}
+                            value={x}
+                            readOnly
+                          />
+                          <button
+                            onClick={() => onDeleteContextId(x)}
+                            className={styles.authorDelete}
+                          />
+                        </div>
                       ))}
                   </>
                 )}
