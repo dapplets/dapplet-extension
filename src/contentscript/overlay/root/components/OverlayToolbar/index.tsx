70c42a260677a66d2fa7fdb1b20f7b9b26d7b467
LiSofa <lisofffa@gmail.com>
2023-03-23 03:21:11 +0300

    fix: remove close icon in tab

--- src/contentscript/overlay/root/components/OverlayToolbar/index.tsx
+++ src/contentscript/overlay/root/components/OverlayToolbar/index.tsx
@@ -370,6 +370,22 @@ export const OverlayToolbar = (p: OverlayToolbarProps): ReactElement => {
 
     return notifications
   }
+  const isModuleActive = () => {
+    if (!p.module || !noSystemTabs.length) return
+
+    let isModuleActive
+
+    p.module
+      .filter((x) => noSystemTabs.filter((i) => i.id === x.name))
+      .map((x) => {
+        if (x.isActive) return (isModuleActive = true)
+        else {
+          return (isModuleActive = false)
+        }
+      })
+
+    return isModuleActive
+  }
 
   return (
     <div
@@ -443,89 +459,71 @@ export const OverlayToolbar = (p: OverlayToolbarProps): ReactElement => {
               (newWidgets && newWidgets.length > 0
                 ? getWigetsConstructor(newWidgets).map((x) => x)
                 : null)}
-
-            <div
-              data-testid={isShowTabs ? 'toolbar-show' : 'toolbar-hide'}
-              className={cn(styles.toggleTabs, {
-                [styles.hideTabs]: !isShowTabs,
-              })}
-            >
-              {noSystemTabs.length > 0 &&
-                noSystemTabs.map((tab) => {
-                  const menuWidgets =
-                    newWidgets &&
-                    newWidgets.length > 0 &&
-                    newWidgets.filter((x) => x.moduleName === tab.id)
-
-                  return (
-                    <OverlayTab
-                      setOpenWallet={p.setOpenWallet}
-                      isOpenWallet={p.isOpenWallet}
-                      key={tab.id}
-                      tabId={tab.id}
-                      {...tab}
-                      isActive={p.activeTabId === tab.id}
-                      activeTabMenuId={p.activeTabMenuId}
-                      onCloseClick={() => p.onCloseClick(tab)}
-                      onMenuClick={(menu) => p.onMenuClick(tab, menu)}
-                      onTabClick={() => p.onTabClick(tab)}
-                      modules={p.module}
-                      pathname={p.pathname}
-                      navigate={p.navigate}
-                      overlays={p.overlays}
-                      onToggleClick={p.onToggleClick}
-                      getWigetsConstructor={getWigetsConstructor}
-                      menuWidgets={menuWidgets}
-                      mainMenuNavigation={p.onMenuClick}
-                    />
-                  )
-                })}
-
-              {/* <ToggleOverlay
-                // getNode={handleClickGetNodeOverlayToolbar}
-                onClick={() => {
-                  if (
-                    document
-                      .querySelector('#dapplets-overlay-manager')
-                      .classList.contains('dapplets-overlay-collapsed')
-                  ) {
-                    p.navigate('/system/dapplets')
-
-                    p.onToggleClick()
-                  } else if (
-                    !document
-                      .querySelector('#dapplets-overlay-manager')
-                      .classList.contains('dapplets-overlay-collapsed')
-                  ) {
-                    if (p.pathname === '/system/dapplets') {
-                      p.onToggleClick()
-                    } else {
-                      p.navigate('/system/dapplets')
-                    }
-                  }
-                }}
-                className={cn(styles.toggleOverlay, {
-                  // [styles.isOpenWallet]: p.isOpenWallet,
-                })}
-              /> */}
-            </div>
-            <div>
-              {noSystemTabs.length > 0 && (
-                <button
-                  onClick={(e) => {
-                    e.preventDefault()
-                    e.stopPropagation()
-                    onShowTabs()
-                  }}
-                  data-testid="show-tabs-button"
-                  className={cn(styles.miniButton, {
-                    [styles.hideTabsBtn]: isShowTabs,
+            {p.module && p.module.length && p.module.filter((x) => x.isActive) && (
+              <>
+                <div
+                  data-testid={isShowTabs ? 'toolbar-show' : 'toolbar-hide'}
+                  className={cn(styles.toggleTabs, {
+                    [styles.hideTabs]: !isShowTabs,
                   })}
                 >
-                  <Show />
-                </button>
-              )}
-            </div>
+                  {noSystemTabs.length > 0 &&
+                    noSystemTabs.map((tab) => {
+                      const menuWidgets =
+                        newWidgets &&
+                        newWidgets.length > 0 &&
+                        newWidgets.filter((x) => x.moduleName === tab.id)
+
+                      return (
+                        <OverlayTab
+                          setOpenWallet={p.setOpenWallet}
+                          isOpenWallet={p.isOpenWallet}
+                          key={tab.id}
+                          tabId={tab.id}
+                          {...tab}
+                          isActive={p.activeTabId === tab.id}
+                          activeTabMenuId={p.activeTabMenuId}
+                          onCloseClick={() => p.onCloseClick(tab)}
+                          onMenuClick={(menu) => p.onMenuClick(tab, menu)}
+                          onTabClick={() => p.onTabClick(tab)}
+                          modules={p.module}
+                          pathname={p.pathname}
+                          navigate={p.navigate}
+                          overlays={p.overlays}
+                          onToggleClick={p.onToggleClick}
+                          getWigetsConstructor={getWigetsConstructor}
+                          menuWidgets={menuWidgets}
+                          mainMenuNavigation={p.onMenuClick}
+                        />
+                      )
+                    })}
+                </div>
+                <div
+                  className={cn(styles.miniButtonBlock, {
+                    [styles.divHidden]:
+                      p.module &&
+                      p.module.length &&
+                      p.module.filter((x) => x.isActive).length === 0,
+                  })}
+                >
+                  {noSystemTabs.length > 0 && (
+                    <button
+                      onClick={(e) => {
+                        e.preventDefault()
+                        e.stopPropagation()
+                        onShowTabs()
+                      }}
+                      data-testid="show-tabs-button"
+                      className={cn(styles.miniButton, {
+                        [styles.hideTabsBtn]: isShowTabs,
+                      })}
+                    >
+                      <Show />
+                    </button>
+                  )}
+                </div>
+              </>
+            )}
           </div>
         </div>
       </div>
