9ef2ce2941f142ea6f3d525beaf51f6f6931e81c
Nikita Teremovskiy <ni-2@mail.ru>
2023-09-25 11:35:34 +0300

    fix: fix CA; return callMethod details

--- src/background/services/connectedAccountService.ts
+++ src/background/services/connectedAccountService.ts
@@ -302,19 +302,33 @@ export default class ConnectedAccountService {
       statement,
     } = props
     const contract = await this._getContract()
-    const requestBody = {
+    const requestBody: {
+      firstAccountId: string
+      firstOriginId: string
+      secondAccountId: string
+      secondOriginId: string
+      isUnlink: boolean
+      firstProofUrl?: string
+      secondProofUrl?: string
+      signature?: EthSignature
+      statement?: string
+    } = {
       firstAccountId,
       firstOriginId,
       secondAccountId,
       secondOriginId,
       isUnlink,
-      firstProofUrl: firstProofUrl === null ? undefined : firstProofUrl,
-      secondProofUrl: secondProofUrl === null ? undefined : secondProofUrl,
-      signature: signature === undefined ? null : signature,
-      statement: statement === null ? undefined : statement,
     }
-    const gas = signature ? 300_000_000_000_000 : null
+    if (firstProofUrl) requestBody.firstProofUrl = firstProofUrl
+    if (secondProofUrl) requestBody.secondProofUrl = secondProofUrl
+    requestBody.signature = signature === undefined ? null : signature
+    if (statement) requestBody.statement = statement
+
+    const gas = signature ? 300_000_000_000_000 : 30_000_000_000_000
     const amount = stake === null ? undefined : stake
+    console.log('requestBody', requestBody)
+    console.log('gas', gas)
+    console.log('amount', amount)
     const res = await contract['requestVerification'](requestBody, gas, amount)
     EventBus.emit('connected_accounts_changed')
     return res
