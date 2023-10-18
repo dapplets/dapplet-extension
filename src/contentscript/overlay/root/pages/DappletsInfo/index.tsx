261348eb4888493a95e1aa74f0054d67f03e842e
Lisofffa <lisofffa@gmail.com>
2023-10-18 23:59:32 +0300

    fix: fixed errors when adding admins

--- src/contentscript/overlay/root/pages/DappletsInfo/index.tsx
+++ src/contentscript/overlay/root/pages/DappletsInfo/index.tsx
@@ -183,6 +183,7 @@ export const DappletsMainInfo: FC<DappletsMainInfoProps> = (props) => {
   }
 
   function containsValue(arr, elem: string) {
+    if (!arr) return
     for (let i = 0; i < arr.length; i++) {
       if (arr[i].toLowerCase() === elem.toLowerCase()) {
         return true
