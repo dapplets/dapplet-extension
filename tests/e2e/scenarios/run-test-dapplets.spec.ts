278703ca09aaafaba06c3d9433a103076592a7ad
Lisofffa <lisofffa@gmail.com>
2023-10-27 14:39:26 +0300

    fix: removed page.pause to tests

--- tests/e2e/scenarios/run-test-dapplets.spec.ts
+++ tests/e2e/scenarios/run-test-dapplets.spec.ts
@@ -37,7 +37,6 @@ for (const dappletName of dapplets) {
         await page.locator('.dapplet-widget').locator(':scope > *').first().click()
         const floatingNotification = await page.getByTestId('notification-label')
         await expect(floatingNotification).toContainText('Test Title')
-        await  page.pause()
         await expect(floatingNotification).toContainText('Test Message')
         await floatingNotification.getByText('show more').click() // opens notifications overlay
         await expect(floatingNotification).not.toBeVisible()
