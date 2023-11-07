38727882857afe07f6d605eb67d97f32433b5297
Alexander Sakhaev <alsakhaev@gmail.com>
2023-11-07 08:15:13 +0000

    feat: show popup when content script is not injected (#236)
    * feat: show popup when content script is not injected
    
    * feat: show different popup for the no script injected case

---
+++ src/popup/index.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+
+import { App } from './App'
+import './index.css'
+
+const container = document.getElementById('app')
+
+ReactDOM.render(<App />, container)
