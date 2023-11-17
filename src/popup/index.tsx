7b6c7c0a65bae17083514a3104574844445c98b5
Lisofffa(lisofffa.near) <lisofffa@gmail.com>
2023-11-17 14:56:16 +0300

    fix: overlay fonts differ on different sites (DAP-4141)
    Co-authored-by: Alexander Sakhaev <alsakhaev@gmail.com>

--- src/popup/index.tsx
+++ src/popup/index.tsx
@@ -4,6 +4,6 @@ import * as ReactDOM from 'react-dom'
 import { App } from './App'
 import './index.css'
 
-const container = document.getElementById('app')
+const container = document.getElementById('dapplets-overlay-react-app')
 
 ReactDOM.render(<App />, container)
