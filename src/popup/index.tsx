497fea9511eacbe19fa6f568fb5c49ea491701be
Alexander Sakhaev <alsakhaev@gmail.com>
2023-11-22 12:36:48 +0100

    fix: extension popup doesn't work

--- src/popup/index.tsx
+++ src/popup/index.tsx
@@ -4,6 +4,6 @@ import * as ReactDOM from 'react-dom'
 import { App } from './App'
 import './index.css'
 
-const container = document.getElementById('dapplets-overlay-react-app')
+const container = document.getElementById('app')
 
 ReactDOM.render(<App />, container)
